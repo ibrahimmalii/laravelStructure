3584e9b6dff27b72b2df02a8b73994fbb60a6b06
Ibrahim Ali <hemamessi47@gmail.com>
2021-07-20 04:16:16 +0200

    add PostResource transformation

--- app/Http/Controllers/Api/postController.php
+++ app/Http/Controllers/Api/postController.php
@@ -6,19 +6,30 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Http\Requests\storePostRequest;
+use App\Http\Resources\PostResource;
 
 class postController extends Controller
 {
     public function index()
     {
         $posts = Post::all();
-        return $posts;
+        // return $posts;
+        return PostResource::collection($posts); //==> without new
     }
 
     public function show($id)
     {
         $post = Post::findOrFail($id);
-        return $post;
+        // return $post;
+
+        //if we want to hide and show data to user
+        // $postArr = [
+        //     'title'=>$post->title,
+        //     'description'=>$post->description,
+        // ];
+
+        //or we can make:resource to reuse it more
+        return new PostResource($post);
     }
 
     public function store(storePostRequest $request)
@@ -28,6 +39,6 @@ class postController extends Controller
             'description' => $request->description,
             'user_id' => $request->createdBy,
         ]);
-        return $post;
+        return new PostResource($post);
     }
 }
